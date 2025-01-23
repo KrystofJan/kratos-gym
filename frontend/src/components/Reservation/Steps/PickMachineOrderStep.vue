146ba96ec578e8a66012f3bffc428825c568b717
Jan-Kryštof Zahradník <105607477+KrystofJan@users.noreply.github.com>
2025-01-23 20:45:22 +0100

    feat: disable order changing after enabling generating (#28)

--- frontend/src/components/Reservation/Steps/PickMachineOrderStep.vue
+++ frontend/src/components/Reservation/Steps/PickMachineOrderStep.vue
@@ -59,6 +59,7 @@ onMounted(() => {
     newSelectedMachines.value = props.selectedMachines
 })
 
+const { value: isDisabled } = useField('generate');
 </script>
 
 <template>
@@ -70,11 +71,13 @@ onMounted(() => {
                   group="people" 
                   @start="drag=true" 
                   @end="drag=false" 
+                  enabled="false"
                   item-key="id"
+                  v-if="!isDisabled"
                   class="flex gap-2 flex-col w-1/3"
                   >
                   <template #item="{element}">
-                    <div class="flex justify-between h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background cursor-grab focus:grabbing">
+                    <div class="flex justify-between h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background cursor-grab focus:cursor-grabbing">
                         <span>
                             {{element.MachineName}}
                         </span>
@@ -82,19 +85,37 @@ onMounted(() => {
                     </div>
                    </template>
                 </draggable>
+
+                <div 
+                  v-else
+                  class="flex gap-2 flex-col w-1/3 opacity-50 cursor-not-allowed"
+                  >
+                  <template
+
+                  v-for="element in newSelectedMachines" 
+                      >
+                    <div class="flex justify-between h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background ">
+                        <span>
+                            {{element.MachineName}}
+                        </span>
+                        <GripHorizontal />
+                    </div>
+                  </template>
+            </div>
                 <KeepAlive>
-                <div class="w-1/3 flex flex-col gap-4">
+                <div class="w-1/3 flex flex-col gap-4" >
                     <FormField v-slot="{ value, handleChange }" type="checkbox" name="generate">
                       <FormItem v-auto-animate class="grid grid-cols-2 -order-1 w-full items-center gap-2 space-y-0">
                         <FormLabel>Should we generate times for you?</FormLabel>
                         <FormControl>
-                          <Checkbox :checked="value" @update:checked="handleChange" />
+                        <Checkbox :checked="value" @update:checked="handleChange" 
+                        />
                         </FormControl>
                         <FormMessage />
                       </FormItem>
                     </FormField>
 
-                    <FormField v-slot="{ value, handleChange }" type="checkbox" name="canDisturb">
+                    <FormField v-slot="{ value, handleChange }" type="checkbox" name="canDisturb" v-if="isDisabled">
                       <FormItem v-auto-animate class="grid grid-cols-2 -order-1 w-full items-center gap-2 space-y-0">
                         <FormLabel>Can someone join you?</FormLabel>
                         <FormControl>
@@ -104,7 +125,7 @@ onMounted(() => {
                       </FormItem>
                     </FormField>
 
-                    <FormField v-slot="{ value, handleChange }" type="checkbox" name="continuous">
+                    <FormField v-slot="{ value, handleChange }" type="checkbox" name="continuous" v-if="isDisabled">
                       <FormItem v-auto-animate class="grid grid-cols-2 -order-1 w-full items-center gap-2 space-y-0">
                         <FormLabel>Can there be gaps between your exercises? TODO: Add maximum gap length</FormLabel>
                         <FormControl>
